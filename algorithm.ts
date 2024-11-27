c5ab9238fadf02951cdb868206295ae7e169529a
John Daniels <jadeyemi@migrapay.com>
2024-11-27 12:38:44 +0100

    update

---
+++ algorithm.ts
@@ -0,0 +1,12 @@
+// Problem: Remove Duplicates from Sorted Array
+// Description:
+
+// Given a sorted array nums, remove the duplicates in place such that each element appears only once and returns the new length.
+
+// You must do this without using extra space (i.e., modify the array in place).
+// After removing the duplicates, the first part of the array should contain the unique elements, and the remaining elements are ignored.
+// Write a function that returns the new length of the array after removing duplicates.
+
+function removeDuplicates(nums: number[]): number {}
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
